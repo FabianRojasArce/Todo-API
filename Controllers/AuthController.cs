f2ed5defa4cb60d7dc82fcb847d6ea5398babc9e
Fabian Rojas <fabian.rojas.arce95@gmail.com>
2023-04-03 03:26:48 -0400

    fix(Auth): se previenen warnings

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -29,39 +29,44 @@ public class AuthController : ControllerBase
     [HttpPost("register")]
     public async Task<IActionResult> Register(RegisterForm form)
     {
-        string userName = form.Email.Split('@')[0];
-        var user = new IdentityUser { Email = form.Email, UserName = userName };
-        var result = await _userManager.CreateAsync(user, form.Password);
-        if (result.Succeeded)
-        {
-            return Ok();
+        if (form.Email != null && form.Password != null && form.ConfirmPassword != null){
+            string userName = form.Email.Split('@')[0];
+            var user = new IdentityUser { Email = form.Email, UserName = userName };
+            var result = await _userManager.CreateAsync(user, form.Password);
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+            foreach (var error in result.Errors)
+            {
+                Console.WriteLine(error.Description);
+            }
+            return BadRequest(result.Errors);
         }
-        foreach (var error in result.Errors)
-        {
-            Console.WriteLine(error.Description);
-        }
-        return BadRequest(result.Errors);
+        return BadRequest();
     }
 
     [HttpPost("login")]
     public async Task<IResult> Login(LoginForm form)
     {
-        var user = await _userManager.FindByEmailAsync(form.Email);
-        if (user == null){
-            return Results.BadRequest();
-        }
-        var password = await _userManager.CheckPasswordAsync(user, form.Password);
-
-        if (password) {
-            if (user.UserName != null){
-                var result = await _signInManager.PasswordSignInAsync(user.UserName, form.Password, true, false);
-                if (result.Succeeded) {
-                    return Results.Ok();
-                }else{
-                    return Results.BadRequest();
-                }
+        if (form.Email != null && form.Password != null){
+            var user = await _userManager.FindByEmailAsync(form.Email);
+            if (user == null){
+                return Results.BadRequest();
             }
+            var password = await _userManager.CheckPasswordAsync(user, form.Password);
 
+            if (password) {
+                if (user.UserName != null){
+                    var result = await _signInManager.PasswordSignInAsync(user.UserName, form.Password, true, false);
+                    if (result.Succeeded) {
+                        return Results.Ok();
+                    }else{
+                        return Results.BadRequest();
+                    }
+                }
+
+            }
         }
         return Results.BadRequest();
     }
@@ -74,14 +79,14 @@ public class AuthController : ControllerBase
     }
 
     public class LoginForm {
-        public string Email {get; set; }
-        public string Password {get; set; }
+        public string? Email {get; set; }
+        public string? Password {get; set; }
     }
 
     public class RegisterForm {
-        public string Email {get; set; }
-        public string Password {get; set; }
-        public string ConfirmPassword {get; set; }
+        public string? Email {get; set; }
+        public string? Password {get; set; }
+        public string? ConfirmPassword {get; set; }
     }
 
 
